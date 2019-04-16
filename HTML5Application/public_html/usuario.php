9addb121e20b6c0a6704b98eb5a1d75a066ef3d2
DabvAstur <cetecom@LCV11009>
2019-04-16 15:37:51 -0300

    Commit #2

--- HTML5Application/public_html/usuario.php
+++ HTML5Application/public_html/usuario.php
@@ -1,8 +1,40 @@
 <?php 
 
-Class Usuario(
+Class Usuario
 {
-    private 
+    private $id, $nombre, $clave;
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getNombre() {
+        return $this->nombre;
+    }
+
+    function getClave() {
+        return $this->clave;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    function setClave($clave) {
+        $this->clave = $clave;
+    }
+
+    function Valida()
+    {
+        if(!isset($mysqli))
+        {
+            return false;
+        }
+    }
 }
 
 ?>
